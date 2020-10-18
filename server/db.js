238be58a22c1bc5319927f0a231d1ca5e4602066
Caleb Dunn <finish.06@gmail.com>
2020-10-18 14:09:24 -0500

    env postgres db

--- server/db.js
+++ server/db.js
@@ -1,10 +1,10 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    user: 'postgres',
-    password: 'postgres',
-    host: 'localhost',
-    post: 5432,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     database: 'perntodo',
 });
 
