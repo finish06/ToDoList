47ceb25651ec0ec0c16d48d8436e0222d5133155
Caleb Dunn <finish.06@gmail.com>
2020-10-31 14:36:06 -0500

    update host & add version variable

--- client/update-registry.sh
+++ client/update-registry.sh
@@ -4,6 +4,17 @@ docker build -t todo-frontend .
 
 # Update local registry
 echo "Tagging container for update to registry"
-docker tag todo-frontend docker.du.nn/todo-frontend:latest
+docker tag todo-frontend 192.168.1.41:25129/todo-frontend:latest
 echo "Pushing container to registry"
-docker push docker.du.nn/todo-frontend:latest
+docker push 192.168.1.41:25129/todo-frontend:latest
+
+if [ -z $1 ]
+then
+    echo "Provide a version #"
+else
+    # Update local registry
+    echo "Tagging container for update to registry"
+    docker tag todo-frontend 192.168.1.41:25129/todo-frontend:$1
+    echo "Pushing container to registry"
+    docker push 192.168.1.41:25129/todo-frontend:$1
+
--- server/update-registry.sh
+++ server/update-registry.sh
@@ -4,6 +4,17 @@ docker build -t todo-backend -f Dockerfile .
 
 # Update local registry
 echo "Tagging container for update to registry"
-docker tag todo-backend docker.du.nn/todo-backend:latest
+docker tag todo-backend 192.168.1.41:25129/todo-backend:latest
 echo "Pushing container to registry"
-docker push docker.du.nn/todo-backend:latest
+docker push 192.168.1.41:25129/todo-backend:latest
+
+if [ -z $1 ]
+then
+    echo "Provide a version #"
+else
+    # Update local registry
+    echo "Tagging container for update to registry"
+    docker tag todo-backend 192.168.1.41:25129/todo-backend:$1
+    echo "Pushing container to registry"
+    docker push 192.168.1.41:25129/todo-backend:$1
+fi
