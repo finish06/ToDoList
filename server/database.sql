4c22adb342294ea64c40e35f20fc6d15f0977fd3
Caleb Dunn <finish.06@gmail.com>
2020-10-31 14:36:47 -0500

    new tables

--- server/database.sql
+++ server/database.sql
@@ -1,6 +1,11 @@
+  
 CREATE DATABASE perntodo;
+
+\c perntodo;
 
 CREATE TABLE todo(
     todo_id SERIAL PRIMARY KEY,
-    description VARCHAR(255)
+    description VARCHAR(255),
+    active_flag BOOLEAN NOT NULL DEFAULT true,
+    created_date timestamp NOT NULL DEFAULT CURRENT_DATE
 );
