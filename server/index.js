ec97ed2cdd7055b01eb6415d13a959ae5ff34123
Caleb Dunn <finish.06@gmail.com>
2020-11-03 06:06:24 -0600

    soft delete

--- server/index.js
+++ server/index.js
@@ -50,8 +50,7 @@ app.put('/todos/:id', async(req, res) => {
     try {
         const {id} = req.params;
         const {description} = req.body;
-        const {activeFlag} = req.body;
-        const updateTodo = await pool.query("UPDATE todo SET description = $1, active_flag = $2 WHERE todo_id = $3", [description, activeFlag, id]);
+        const updateTodo = await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $3", [description, id]);
         res.json('Todo was updated');
     } catch (err) {
         console.error(err.message);
@@ -62,7 +61,7 @@ app.put('/todos/:id', async(req, res) => {
 app.delete('/todos/:id',  async (req, res) => {
     try {
         const {id} = req.params;
-        const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [id]);
+        const deleteTodo = await pool.query("UPDATE todo SET active_flag = false WHERE todo_id = $1", [id]);
         res.json('Todo was deleted');
     } catch (err) {
         console.error(err.message);
