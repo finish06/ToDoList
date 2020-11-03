c3d32cc46c54c931aab2f45f078f4aa6491d6766
Caleb Dunn <finish.06@gmail.com>
2020-11-03 05:50:26 -0600

    query only active flags

--- server/index.js
+++ server/index.js
@@ -38,7 +38,7 @@ app.get("/todos", async(req, res) => {
 app.get('/todos/:id', async(req, res) => {
     try {
         const {id} = req.params;
-        const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id])
+        const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1 AND active_flag = True", [id])
         res.json(todo.rows[0])
     } catch (err) {
         console.error(err.message)
